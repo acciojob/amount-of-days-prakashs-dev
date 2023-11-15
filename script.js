19c8b22c3c0bee04ad8040217ed09e51b873e825
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-15 09:28:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+	if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+	    return 366;
+	} else {
+	    return 365;
+	}
+}
+const year = Number(prompt("Enter a year"));
+alert(daysOfAYear(year));
