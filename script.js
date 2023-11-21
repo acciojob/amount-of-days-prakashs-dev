a0e8d0b6f7e1c3db9d4f7adf4efac78aabb17e85
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-21 18:55:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
 function daysOfAYear(year) {
 	if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
 	    return 366;
-	} else {
-	    return 365;
-	}
+	return 365;
 }
-const year = Number(prompt("Enter a year"));
-alert(daysOfAYear(year));
+// const year = Number(prompt("Enter a year"));
+// alert(daysOfAYear(year));
